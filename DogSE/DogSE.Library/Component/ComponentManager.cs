6da443a578dbc45173752a469968a760e15a0f32
dogvane <dogvane@gmail.com>
2013-08-08 17:17:18 +0800

    增加组件模式的模块代码

---
+++ DogSE/DogSE.Library/Component/ComponentManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace DogSE.Library.Component
+{
+    /// <summary>
+    /// 组件管理器的实现模式
+    /// </summary>
+    public class ComponentManager:IComponentManager
+    {
+        private readonly ConcurrentDictionary<string, object> m_ComponentDictionary =
+            new ConcurrentDictionary<string, object>();
+
+        /// <summary>
+        /// 注册一个组件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="componentId"></param>
+        /// <param name="component"></param>
+        public void RegisterComponent<T>(string componentId, T component) where T : class
+        {
+            if (string.IsNullOrEmpty(componentId))
+                throw new ArgumentNullException("componentId");
+
+            if (component == null)
+                throw new ArgumentNullException("component");
+
+            m_ComponentDictionary[componentId] = component;
+        }
+
+        /// <summary>
+        /// 获得一个组件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="componentId"></param>
+        /// <returns></returns>
+        public T GetComponent<T>(string componentId) where T : class
+        {
+            if (string.IsNullOrEmpty(componentId))
+                throw new ArgumentNullException("componentId");
+
+            object value;
+            if (m_ComponentDictionary.TryGetValue(componentId, out value))
+                return value as T;
+
+            return null;
+        }
+
+        /// <summary>
+        /// 释放组件资源
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public void ReleaseComponent<T>()
+        {
+            m_ComponentDictionary.Clear();
+        }
+    }
+}
---
+++ DogSE/DogSE.Library/Component/IComponentManager.cs
@@ -0,0 +1,30 @@
+﻿namespace DogSE.Library.Component
+{
+    /// <summary>
+    /// 组件模式模块
+    /// </summary>
+    public interface IComponentManager
+    {
+        /// <summary>
+        /// 注册一个组件数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="componentId"></param>
+        /// <param name="component"></param>
+        void RegisterComponent<T>(string componentId, T component) where T : class;
+
+        /// <summary>
+        /// 获得一个组件数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="componentId"></param>
+        /// <returns></returns>
+        T GetComponent<T>(string componentId) where T : class;
+
+        /// <summary>
+        /// 释放组件的资源
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        void ReleaseComponent<T>();
+    }
+}
