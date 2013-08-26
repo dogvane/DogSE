8fa494478a4b5d5fd18ce23c00a7ba336f80f7d4
dogvane <dogvane@gmail.com>
2013-08-26 18:30:56 +0800

    1.增加线程队列功能
    Signed-off-by: dogvane <dogvane@gmail.com>

---
+++ DogSE/DogSE.Library/Thread/ThreadQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DogSE.Library.Thread;
+
+namespace Org.JQueen.Sanguo.GameWorld.Server.Common
+{
+    /// <summary>
+    /// 线程队列，用于按照顺序执行方法
+    /// 只用一个线程来执行队列，多线程时自己使用系统提供的线程池
+    /// </summary>
+    public static class ThreadQueue
+    {
+        static private readonly ThreadQueueEntity normalThreadQueue = new ThreadQueueEntity("normalThreadQueue");
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="method"></param>
+        public static void Append(Action method)
+        {
+            normalThreadQueue.Append(method);
+        }
+
+        static private readonly ThreadQueueEntity ioThreadQueue = new ThreadQueueEntity("ioThreadQueue");
+
+        /// <summary>
+        /// 加入一个IO操作队列
+        /// </summary>
+        /// <param name="method"></param>
+        public static void AppendIO(Action method)
+        {
+            ioThreadQueue.Append(method);
+        }
+
+
+        /// <summary>
+        /// 当前队列里是否还有正在执行的任务
+        /// </summary>
+        public static bool HasQueues
+        {
+            get { return normalThreadQueue.HasQueues | ioThreadQueue.HasQueues; }
+        }
+    }
+}
---
+++ DogSE/DogSE.Library/Thread/ThreadQueueEntity.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using DogSE.Library.Log;
+
+namespace DogSE.Library.Thread
+{
+    /// <summary>
+    /// 线程队列实例
+    /// </summary>
+    /// <remarks>
+    /// 这里是已实例情况存在
+    /// 目的时，如果有新的扩展时，可以在静态类里，自己创建一个静态对象。
+    /// 方便后面的代码使用
+    /// </remarks>
+    class ThreadQueueEntity
+    {
+        /// <summary>
+        /// 创建队列需要输入名字
+        /// </summary>
+        /// <param name="queueName"></param>
+        public ThreadQueueEntity(string queueName)
+        {
+            _queueName = queueName;
+        }
+
+        private readonly string _queueName = string.Empty;
+
+        readonly Queue<Action> Methods = new Queue<Action>();
+        volatile bool _isLockQueue;
+
+        /// <summary>
+        /// 增加一个异步任务
+        /// </summary>
+        /// <param name="method"></param>
+        public void Append(Action method)
+        {
+            if (method == null)
+                return;
+
+            bool isLock = false;
+            Action[] writeArray = null;
+            lock (Methods)
+            {
+                Methods.Enqueue(method);
+
+                // 检测是否有其它的线程已在处理中，如在使用就退出,否则开始锁定
+                if (_isLockQueue == false)
+                {
+                    isLock = _isLockQueue = true;
+                    writeArray = Methods.ToArray();
+                    Methods.Clear();
+                }
+            }
+
+            // 如果有其它的线程在处理就退出
+            if (isLock == false)
+                return;
+
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                while (writeArray.Length > 0)
+                {
+                    Stopwatch watch = Stopwatch.StartNew();
+                    foreach (var m in writeArray)
+                    {
+                        try
+                        {
+                            watch.Restart();
+
+                            m();
+
+                            watch.Stop();
+
+                            //  TODO 这里要考虑如何记录时间
+                        }
+                        catch (Exception ex)
+                        {
+                            Logs.Error("Run thread queue({0}) method error.", _queueName, ex);
+                        }
+
+                    }
+
+                    watch.Stop();
+
+                    lock (Methods)
+                    {
+                        //  这里在执行完一次操作后，需要返回重新读一次队列，如果队列里有数据，则还需要进行后面的处理
+                        writeArray = Methods.ToArray();
+                        Methods.Clear();
+                    }
+                }
+
+                _isLockQueue = false;
+            });
+        }
+
+
+
+        /// <summary>
+        /// 当前队列里是否还有正在执行的任务
+        /// </summary>
+        public bool HasQueues
+        {
+            get
+            {
+                lock (Methods)
+                {
+                    if (Methods.Count > 0)
+                        return true;
+                }
+
+                return _isLockQueue;
+            }
+        }
+    }
+}
