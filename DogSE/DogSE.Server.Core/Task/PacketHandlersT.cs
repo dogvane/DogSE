650d3caa95c0ee561eb5445edb191254bc156efc
dogvane <dogvane@gmail.com>
2015-09-15 15:44:38 +0800

    1.增加一个聊天室的服务端例子代码

---
+++ DogSE/DogSE.Server.Core/Task/PacketHandlersT.cs
@@ -0,0 +1,261 @@
+﻿
+//     NOTES
+// ---------------
+//
+// This file is a part of the MMOSE(Massively Multiplayer Online Server Engine) for .NET.
+//
+//                              2006-2010 DemoSoft Team
+//
+//
+// First Version : by H.Q.Cai - mailto:caihuanqing@hotmail.com
+// Update Version: by Dogvane - mailto:dogvane@gmail.com
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published
+ *   by the Free Software Foundation; either version 2.1 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
+#region zh-CHS 包含名字空间 | en Include namespace
+
+using System;
+using DogSE.Library.Log;
+using DogSE.Server.Core.Net;
+using DogSE.Server.Core.Protocol;
+
+#endregion
+
+namespace DogSE.Server.Core.Task
+{
+    /// <summary>
+    /// 管理全部数据包的调用者
+    /// </summary>
+    public class PacketHandlersBaseT<T>
+    {
+
+        #region zh-CHS 公开方法 | en Public Method
+        #region zh-CHS 私有成员变量 | en Private Member Variables
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly PacketHandlerT<T>[] m_Handlers = new PacketHandlerT<T>[ushort.MaxValue];
+
+        /// <summary>
+        /// 仅用于测试
+        /// </summary>
+        public PacketHandlerT<T>[] Handlers
+        {
+            get
+            {
+                return m_Handlers;
+            }
+        }
+        #endregion
+        /// <summary>
+        /// 注册数据包的处理调用者
+        /// 注意，如果存在相同的消息id，会进行调用方法的替换操作
+        /// </summary>
+        /// <param name="iPacketID"></param>
+        /// <param name="onPacketReceive"></param>
+        public void Register(ushort iPacketID, Action<T, PacketReader> onPacketReceive)
+        {
+            if (m_Handlers[iPacketID] != null)
+            {
+                //  如果有注册相同的消息id，这里只是进行记录，并不干预运行
+                Logs.Warn("Msgid {0} is replace.", iPacketID);
+            }
+            
+            //  这里在初始化的时候就把对应的性能监视的对象给创建好
+            NetTaskProfile.GetNetTaskProfile(iPacketID);
+
+            m_Handlers[iPacketID] = new PacketHandlerT<T>(iPacketID, PacketPriority.Normal, onPacketReceive);
+        }
+
+        /// <summary>
+        /// 任务类型
+        /// </summary>
+        /// <param name="iPacketID"></param>
+        /// <param name="taskType"></param>
+        /// <param name="onPacketReceive"></param>
+        public void Register(ushort iPacketID, TaskType taskType, Action<T, PacketReader> onPacketReceive)
+        {
+            if (m_Handlers[iPacketID] != null)
+            {
+                //  如果有注册相同的消息id，这里只是进行记录，并不干预运行
+                Logs.Warn("Msgid {0} is replace.", iPacketID);
+            }
+
+            //  这里在初始化的时候就把对应的性能监视的对象给创建好
+            NetTaskProfile.GetNetTaskProfile(iPacketID);
+
+            m_Handlers[iPacketID] = new PacketHandlerT<T>(iPacketID, PacketPriority.Normal, taskType, onPacketReceive);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="iPacketID"></param>
+        /// <param name="priority"></param>
+        /// <param name="onPacketReceive"></param>
+        public void Register(ushort iPacketID, PacketPriority priority, Action<T, PacketReader> onPacketReceive)
+        {
+            if (m_Handlers[iPacketID] != null)
+            {
+                Logs.Warn("Msgid {0} is replace.", iPacketID);
+            }
+
+            NetTaskProfile.GetNetTaskProfile(iPacketID);
+
+            m_Handlers[iPacketID] = new PacketHandlerT<T>(iPacketID, priority, onPacketReceive);
+        }
+
+
+        /// <summary>
+        /// 获取数据包的处理调用者
+        /// </summary>
+        /// <param name="iPacketID"></param>
+        /// <returns>
+        /// 如果消息id对应的消息不存在，则会返回null
+        /// </returns>
+        public PacketHandlerT<T> GetHandler(ushort iPacketID)
+        {
+            return m_Handlers[iPacketID];
+        }
+
+        /// <summary>
+        /// 移去数据包的处理调用者
+        /// </summary>
+        /// <param name="iPacketID"></param>
+        internal void RemoveHandler(ushort iPacketID)
+        {
+            m_Handlers[iPacketID] = null;
+        }
+
+        /// <summary>
+        /// 清理所有的消息处理句柄
+        /// </summary>
+        public void Clean()
+        {
+            for (int i = 0; i < m_Handlers.Length; i++)
+            {
+                m_Handlers[i] = null;
+            }
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// 数据包的主处理者
+    /// </summary>
+    public class PacketHandlerT<T>
+    {
+        #region zh-CHS 构造和初始化和清理 | en Constructors and Initializers and Dispose
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="iPacketID"></param>
+        /// <param name="priority"></param>
+        /// <param name="onPacketReceive"></param>
+        internal PacketHandlerT(ushort iPacketID, PacketPriority priority, Action<T, PacketReader> onPacketReceive)
+        {
+            m_PacketID = iPacketID;
+            m_PacketPriority = priority;
+            m_OnReceive = onPacketReceive;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="iPacketID"></param>
+        /// <param name="priority"></param>
+        /// <param name="taskType"></param>
+        /// <param name="onPacketReceive"></param>
+        internal PacketHandlerT(ushort iPacketID, PacketPriority priority, TaskType taskType,
+            Action<T, PacketReader> onPacketReceive)
+        {
+            m_PacketID = iPacketID;
+            m_PacketPriority = priority;
+            m_OnReceive = onPacketReceive;
+            m_TaskType = taskType;
+        }
+
+        #endregion
+
+        #region zh-CHS 属性 | en Properties
+        #region zh-CHS 私有成员变量 | en Private Member Variables
+        /// <summary>
+        /// 数据包的ID
+        /// </summary>
+        private readonly ushort m_PacketID;
+        #endregion
+        /// <summary>
+        /// 数据包的ID
+        /// </summary>
+        public ushort PacketID
+        {
+            get { return m_PacketID; }
+        }
+
+        #region zh-CHS 私有成员变量 | en Private Member Variables
+        /// <summary>
+        /// 数据包的优先级
+        /// </summary>
+        private readonly PacketPriority m_PacketPriority = PacketPriority.Normal;
+        #endregion
+        /// <summary>
+        /// 数据包的优先级
+        /// </summary>
+        public PacketPriority PacketPriority
+        {
+            get { return m_PacketPriority; }
+        }
+
+        #endregion
+
+        #region zh-CHS 私有成员变量 | en Private Member Variables
+
+
+        #region zh-CHS 私有成员变量 | en Private Member Variables
+
+        /// <summary>
+        /// 数据实际处理的回调
+        /// </summary>
+        private readonly Action<T, PacketReader> m_OnReceive;
+
+        #endregion
+
+        /// <summary>
+        /// 数据实际处理的回调
+        /// </summary>
+        public Action<T, PacketReader> OnReceive
+        {
+            get { return m_OnReceive; }
+        }
+
+
+        #endregion
+
+        #region zh-CHS 私有成员变量 | en Private Member Variables
+
+        /// <summary>
+        /// 数据包的优先级
+        /// </summary>
+        private readonly TaskType m_TaskType = TaskType.Main;
+
+        /// <summary>
+        /// 数据包的优先级
+        /// </summary>
+        public TaskType TaskType
+        {
+            get { return m_TaskType; }
+        }
+
+        #endregion
+    }
+}
+
---
+++ Example/Example2/Program.cs
@@ -0,0 +1,258 @@
+﻿using DogSE.Library.Log;
+using DogSE.Server.Core.Net;
+using DogSE.Server.Core.Task;
+using DogSE.Server.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Example2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Logs.AddConsoleAppender();
+
+            //  注册网络消息
+            packetHandlersManager.Register((ushort) OpCode.Login, OnLogin);
+            packetHandlersManager.Register((ushort)OpCode.SendMessage, OnSendMessage);
+            packetHandlersManager.Register((ushort)OpCode.RecvPrivateMessage, OnSendPrivateMessage);
+
+            var servers = new Listener<Session>();
+            servers.SocketConnect += OnSocketConnect;
+            servers.SocketDisconnect += OnSocketDisconnect;
+            servers.SocketRecv += OnSocketRecv;
+            servers.StartServer(10086);
+
+            Logs.Info("服务器启动，等待客户端连接。按Esc键退出");
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape)
+                    break;
+
+                Thread.Sleep(100);
+            }
+        }
+
+        /// <summary>
+        /// socket连接
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void OnSocketConnect(object sender, SocketConnectEventArgs<Session> e)
+        {
+            //  互相绑定
+            var s = new Session();
+            s.Client = e.Session;
+            e.Session.Data = s;
+
+            nologinSessions.Add(s);
+
+        }
+
+        /// <summary>
+        /// Socket关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void OnSocketDisconnect(object sender, SocketDisconnectEventArgs<Session> e)
+        {
+            var s = e.Session.Data;
+            
+            if (s.IsLogin)
+                sessions.Remove(s);
+            else
+                nologinSessions.Remove(s);
+
+            //  解除互相的引用关系
+            s.Client = null;
+            e.Session.Data = null;
+        }
+
+        /// <summary>
+        /// 收到Socket数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void OnSocketRecv(object sender, SocketRecvEventArgs<Session> e)
+        {
+            var session = e.Session.Data;
+            session.RQ.Enqueue(e.Buffer.Bytes, 0, e.Buffer.Length);
+
+            var packetlen = session.RQ.GetPacketLength();
+            while (packetlen >= session.RQ.Length)
+            {
+                var dogBuffer = new DogBuffer();
+                session.RQ.Dequeue(dogBuffer.Bytes, 0, packetlen);
+
+                var reader = new PacketReader();
+                reader.SetBuffer(dogBuffer);
+
+                /*
+                var pid = (OpCode)reader.GetPacketID();
+
+                switch (pid)
+                {
+                    case OpCode.Login:
+                        OnLogin(session, reader);
+                        break;
+                    case OpCode.SendMessage:
+                        OnSendMessage(session, reader);
+                        break; 
+                    case OpCode.SendPriviteMessage:
+                        OnSendPrivateMessage(session, reader);
+                        break;
+                    default:
+                        Logs.Error("未知消息ID {0}", (int)pid);
+                        break;
+                }
+                */
+
+                var pid = reader.GetPacketID();
+                var handler = packetHandlersManager.GetHandler(pid);
+                if (handler == null)
+                {
+                    Logs.Error("未知消息ID {0}", pid);
+                }
+                else
+                {
+                    handler.OnReceive(session, reader);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 客户端连接列表
+        /// </summary>
+        private static readonly List<Session> sessions = new List<Session>();
+
+        /// <summary>
+        /// 未验证过的Session
+        /// </summary>
+        private static readonly List<Session> nologinSessions = new List<Session>();
+
+
+        private static readonly PacketHandlersBaseT<Session> packetHandlersManager = new PacketHandlersBaseT<Session>();
+
+        /// <summary>
+        /// 登录服务器
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="reader"></param>
+        static void OnLogin(Session session, PacketReader reader)
+        {
+            var userName = reader.ReadUTF8String();
+            var pwd = reader.ReadUTF8String();
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                Logs.Error("连接的用户名是空");
+                session.Client.CloseSocket();
+            }
+
+            if (pwd != "123")
+            {
+                Logs.Error("用户名 {0} 速度的密码错误", userName);
+                var writer = new PacketWriter();
+                writer.SetNetCode((ushort)OpCode.LoginResult);
+                writer.Write(1);    //  0表示登录成功 1表示密码错误
+                session.Client.SendPackage(writer.GetBuffer());
+                return;
+            }
+
+            //  如果玩家之前登录过，则把之前的客户端踢下线
+            var exists = sessions.FirstOrDefault(o => o.Name == userName);
+            if (exists != null)
+            {                
+                exists.IsLogin = false;
+                sessions.Remove(exists);
+                exists.Client.CloseSocket();
+            }
+
+            //  登录完成
+            session.IsLogin = true;
+            nologinSessions.Remove(session);
+            sessions.Add(session);
+
+            session.Name = userName;
+            session.Pwd = pwd;
+        }
+
+        /// <summary>
+        /// 给聊天室里的人都发消息
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="reader"></param>
+        static void OnSendMessage(Session session, PacketReader reader)
+        {
+            var message = reader.ReadUTF8String();
+            if (string.IsNullOrEmpty(message))
+            {
+                //  空消息
+                return;
+            }
+
+            //  广播给所有在线的用户
+            var writer = new PacketWriter();
+            writer.SetNetCode((ushort)OpCode.RecvMessage);
+            foreach (var ss in sessions)
+            {
+                ss.Client.SendPackage(writer.GetBuffer());
+            }
+        }
+
+        /// <summary>
+        /// 给聊天室里的某人单独发消息
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="reader"></param>
+        static void OnSendPrivateMessage(Session session, PacketReader reader)
+        {
+            var userName = reader.ReadUTF8String();
+            var message = reader.ReadUTF8String();
+
+            if (message == null)
+                return;
+
+            var target = sessions.FirstOrDefault(o => o.Name == userName);
+            if (target == null)
+                return;
+
+            var writer = new PacketWriter();
+            writer.SetNetCode((ushort) OpCode.RecvPrivateMessage);
+            writer.WriteUTF8Null(session.Name);
+            writer.WriteUTF8Null(message);
+
+            target.Client.SendPackage(writer.GetBuffer());
+        }
+
+    }
+
+    public class Session
+    {
+        public Session()
+        {
+            RQ = new ReceiveQueue();
+        }
+
+        public string Name { get; set; }
+
+        public string Pwd { get; set; }
+
+        /// <summary>
+        /// 是否已经登录验证过
+        /// </summary>
+        public bool IsLogin { get; set; }
+
+        public ClientSession<Session> Client { get; set; }
+
+        public ReceiveQueue RQ { get; set; }
+    
+
+    }
+}
