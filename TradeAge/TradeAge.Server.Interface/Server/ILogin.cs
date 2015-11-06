2176053a3cd4c777ca0b43ffda16dc2d395b4a2c
dogvane <dogvane@gmail.com>
2015-11-06 19:56:11 +0800

    1.提交遗漏文件

---
+++ TradeAge/TradeAge.Server.Interface/Server/ILogin.cs
@@ -0,0 +1,36 @@
+﻿using DogSE.Server.Core.LogicModule;
+using DogSE.Server.Core.Net;
+using DogSE.Server.Core.Protocol;
+using TradeAge.Server.Entity.Character;
+using TradeAge.Server.Entity.NetCode;
+
+namespace TradeAge.Server.Interface.Server
+{
+    /// <summary>
+    /// 登陆服务器模块
+    /// </summary>
+    public interface ILogin : ILogicModule
+    {
+        /// <summary>
+        /// 登陆服务器
+        /// </summary>
+        /// <param name="netstate"></param>
+        /// <param name="accountName"></param>
+        /// <param name="password"></param>
+        /// <param name="serverId">服务器id</param>
+        [NetMethod((ushort) OpCode.LoginServer, NetMethodType.SimpleMethod, false)]
+        void OnLoginServer(NetState netstate, string accountName, string password, int serverId = 0);
+
+        //void OnLoginServer(NetState netsatte, PacketReader reader);
+
+        /// <summary>
+        /// 创建角色
+        /// </summary>
+        /// <param name="netstate"></param>
+        /// <param name="playerName"></param>
+        /// <param name="sex">性别</param>
+        [NetMethod((ushort)OpCode.CreatePlayerResult, NetMethodType.SimpleMethod)]
+        void OnCreatePlayer(NetState netstate, string playerName, Sex sex);
+
+    }
+}
---
+++ TradeAge/TradeAge.Server.Interface/Server/IScene.cs
@@ -0,0 +1,23 @@
+﻿using DogSE.Server.Core.LogicModule;
+using DogSE.Server.Core.Net;
+using DogSE.Server.Core.Protocol;
+using TradeAge.Server.Entity.Common;
+using TradeAge.Server.Entity.NetCode;
+
+namespace TradeAge.Server.Interface.Server
+{
+    /// <summary>
+    /// 场景业务逻辑
+    /// </summary>
+    public interface IScene : ILogicModule
+    {
+        /// <summary>
+        /// 玩家通报一次移动
+        /// </summary>
+        /// <param name="netstate">网络客户端</param>
+        /// <param name="postion">当前位置</param>
+        /// <param name="direction">朝向（方向以及速度）</param>
+        [NetMethod((ushort)OpCode.OnMove, NetMethodType.SimpleMethod)]
+        void OnMove(NetState netstate, Vector3 postion, Vector3 direction);
+    }
+}
