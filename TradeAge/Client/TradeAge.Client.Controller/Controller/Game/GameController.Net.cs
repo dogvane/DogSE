18e7548b5b2eb0051523b0a9c482212e5f54ef75
dogvane <dogvane@gmail.com>
2015-12-08 17:20:39 +0800

    1补一个漏的文件

---
+++ TradeAge/Client/TradeAge.Client.Controller/Controller/Game/GameController.Net.cs
@@ -0,0 +1,72 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DogSE.Client.Core;
+using DogSE.Client.Core.Net;
+using DogSE.Client.Core.Task;
+
+namespace TradeAge.Client.Controller.Game
+{
+
+
+    partial class GameController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="net"></param>
+        public GameController(NetController net)
+        {
+            nc = net;
+            new ControllerPacketHandler(net, this);
+        }
+
+        private NetController nc;
+
+
+        private NetState NetState
+        {
+            get
+            {
+                return nc.NetState;
+            }
+        }
+
+
+
+        class ControllerPacketHandler
+        {
+            public ControllerPacketHandler(NetController net, BaseGameController logic)
+            {
+                PacketHandlerManager = net.PacketHandlers;
+
+                module = logic;
+PacketHandlerManager.Register(2, OnSyncServerTime);
+
+            }
+
+        public PacketHandlersBase PacketHandlerManager {get;set;}
+
+        BaseGameController module;
+
+void OnSyncServerTime(NetState netstate, PacketReader reader){
+var p1 = new DateTime(reader.ReadLong64());
+var p2 = reader.ReadInt32();
+module.OnSyncServerTime(p1,p2);
+}
+
+
+
+        }
+
+
+
+    }
+
+
+}
+
+
